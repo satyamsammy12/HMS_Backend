1ae2f22405d8121cd80638c6e9760b03584b040b
Satyam sen <68012257+satyamsammy12@users.noreply.github.com>
2024-10-15 19:30:08 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,6 +9,9 @@ export const generateToken = (user, message, statusCode, res) => {
         expires: new Date(
           Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
         ),
+        httpOnly:true,
+        secure:true,
+        sameSite:"None"
       })
       .json({
         success: true,
